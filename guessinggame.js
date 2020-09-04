67664cd2cbe10f4fa12f26af1f6df7cb8370d65b
Taiwo Adepoju <tadepoju95@gmail.com>
2020-09-04 12:30:44 -0500

    Modified all files in the project folder.

--- guessinggame.js
+++ guessinggame.js
@@ -1,16 +1,30 @@
 let guessNumber = () => {
-  let count = 0;
-  count++
+  let count = document.getElementById('increaseOne').innerHTML;
+  count++;
+  document.getElementById('increaseOne').innerHTML = count;
   let userInput = document.getElementById('getNumbers').value;
   let randomNumber = Math.floor(Math.random() * Math.floor(100)); 
-  if(userInput < randomNumber) {
-    document.getElementById('userNumber').innerHTML = 'guess too low';
-  } else if(userInput > randomNumber) {
-    document.getElementById('userNumber').innerHTML = 'guess too high';
-  } else if(count > 3 && userInput > randomNumber || userInput < randomNumber) {
+  console.log(randomNumber);
+  if(count === 3 && randomNumber < userInput ) {
     document.getElementById('btn').disabled = true;
     document.getElementById('result').innerHTML = 'computer chose : ' + randomNumber;
+    document.getElementById('userNumber').innerHTML = 'guess too high';
+
+  } else if(count === 3 && randomNumber > userInput) {
+    document.getElementById('userNumber').innerHTML = 'guess too low';
+    document.getElementById('btn').disabled = true;
+    document.getElementById('result').innerHTML = 'computer chose : ' + randomNumber;
+
+  } else if(randomNumber < userInput) {
+    document.getElementById('userNumber').innerHTML = 'guess too high';
+
+  } else if(randomNumber > userInput) {
+    document.getElementById('userNumber').innerHTML = 'guess too low';
+
+  } else if (randomNumber === userInput) {
+    document.getElementById('userNumber').innerHTML = 'you are correct';
   }
 }
 
-// || userInput < randomNumber
+
+
